b629cf602a4bb7e31f614567b02d2ea0e5757bc1
lh2nw <lh2nw@umsystem.edu>
2026-02-04 14:00:04 -0600

    Add files via upload

---
+++ Kirby/Kirby.py
@@ -0,0 +1,49 @@
+from random import randint
+
+WIDTH = 400
+HEIGHT = 400
+score = 0
+game_over = False
+kirby = Actor("kirby")
+kirby.pos = 100, 100
+coin = Actor("coin")
+coin.pos = 200, 200
+
+def draw():
+    screen.fill("green")
+    kirby.draw()
+    coin.draw()
+    screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Final Score: " + str(score), topleft=(10, 10), fontsize=60)
+
+def place_coin():
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT - 20))
+
+
+def time_up():
+    global game_over
+    game_over = True
+
+def update():
+    global score
+    if keyboard.left:
+        kirby.x = kirby.x - 2
+    elif keyboard.right:
+        kirby.x = kirby.x + 2
+    elif keyboard.up:
+        kirby.y = kirby.y - 2
+    elif keyboard.down:
+        kirby.y = kirby.y + 2
+
+    coin_collected = kirby.colliderect(coin)
+
+    if coin_collected:
+        score = score + 10
+        place_coin()
+
+clock.schedule(time_up, 20.0)
+place_coin()
